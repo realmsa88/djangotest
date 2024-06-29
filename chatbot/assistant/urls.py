5dc92ac52e558bc09d001a5b1cbc632383ccb647
MIFZAL FAUZI <mifzal.aqil@gmail.com>
2024-06-29 18:22:13 +0800

    latest-commit

--- chatbot/assistant/urls.py
+++ chatbot/assistant/urls.py
@@ -4,7 +4,7 @@ from . import views, pipeline, zendesk
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('', views.chat, name='chat'),
+    path('chat/', views.chat, name='chat'),
     path('test/', pipeline.check, name='check'),
     path('zendesk/', zendesk.zendesk, name='zendesk')
     # path('pass_to_admin/', views.pass_to_admin, name='pass_to_admin'),
